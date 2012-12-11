916702b87f6ae2cdd086401f6215d466deb83b0d
Svilborg <svilborg@gmail.com>
2012-12-11 17:27:50 +0200

    Libs

--- src/comix/bgteams/app/BgTeamsWallpaperService.java
+++ src/comix/bgteams/app/BgTeamsWallpaperService.java
@@ -1,21 +1,31 @@
 package comix.bgteams.app;
 
+import javax.microedition.khronos.opengles.GL10;
+
+import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.handler.physics.PhysicsHandler;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.modifier.LoopEntityModifier;
+import org.anddev.andengine.entity.modifier.RotationModifier;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
 import org.anddev.andengine.extension.ui.livewallpaper.BaseLiveWallpaperService;
+import org.anddev.andengine.opengl.texture.ITexture;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.BaseTexture.ITextureStateListener;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.opengl.texture.source.ITextureSource;
+import org.anddev.andengine.opengl.util.GLHelper;
 import org.anddev.andengine.util.Debug;
 
 import android.content.Context;
@@ -24,6 +34,7 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements SharedPreferences.OnSharedPreferenceChangeListener {
 	// ===========================================================
@@ -37,7 +48,7 @@ public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements
 	private static final int CAMERA_WIDTH = 720;
 	private static final int CAMERA_HEIGHT = 480;
 
-	private static final float DEMO_VELOCITY = 100.0f;
+	private static final float DEMO_VELOCITY = 30.0f;
 
 	// ===========================================================
 	// Fields
@@ -49,6 +60,7 @@ public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements
 	private Texture mTexture;
 
 	private TiledTextureRegion mFaceTextureRegion;
+	private TextureRegion mTextureRegion;
 
 	// ===========================================================
 	// Constructors
@@ -76,8 +88,12 @@ public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements
 		// CAMERA_WIDTH = width;
 		// CAMERA_HEIGHT = height;
 
+		// Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		// return new org.anddev.andengine.engine.Engine(new EngineOptions(true,
+		// ScreenOrientation.PORTRAIT, new FillResolutionPolicy(), mCamera));
+
 		Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		return new org.anddev.andengine.engine.Engine(new EngineOptions(true, ScreenOrientation.PORTRAIT, new FillResolutionPolicy(), mCamera));
+		return new org.anddev.andengine.engine.Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera));
 	}
 
 	@Override
@@ -94,19 +110,123 @@ public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements
 	public void onSharedPreferenceChanged(SharedPreferences pSharedPrefs, String pKey) {
 
 	}
+	
+
 
 	@Override
 	public void onLoadResources() {
-		//this.mTexture = new Texture(32, 32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-//		this.mFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "gfx/face_box.png", 0, 0, 0, 1);
+		// this.mTexture = new Texture(32, 32,
+		// TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		// this.mFaceTextureRegion =
+		// TextureRegionFactory.createTiledFromAsset(this.mTexture, this,
+		// "gfx/face_box.png", 0, 0, 0, 1);
 
-		//		this.mFaceTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/face_box.png");
+		// this.mFaceTextureRegion =
+		// TextureRegionFactory.createFromAsset(this.mTexture, this,
+		// "gfx/face_box.png");
+
+		// this.mTexture = new Texture(256, 256, TextureOptions.DEFAULT);
+		// this.mFaceTextureRegion =
+		// TextureRegionFactory.createTiledFromAsset(this.mTexture, this,
+		// "gfx/bfs.png", 0, 0, 1, 1);
+		//
+		// this.mEngine.getTextureManager().loadTexture(this.mTexture);
+
+		// this.mTexture = new Texture(256, 256,
+		// TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		// this.mTextureRegion =
+		// TextureRegionFactory.createFromAsset(this.mTexture, this,
+		// "gfx/bfs.png", 0, 0);
+		//
+		// this.mEngine.getTextureManager().loadTexture(this.mTexture);
+
+//		this.mTexture = new Texture(64, 32, TextureOptions.DEFAULT);
+//
+//		this.mTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/face_box.png", 0, 0);
+//		this.mEngine.getTextureManager().loadTexture(this.mTexture);
 		
-		this.mTexture = new Texture(64, 64, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		this.mFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "gfx/face_box.png", 0, 0, 1, 1);
+		this.mTexture = new Texture(32, 32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/face_box.png", 0, 0);
 
 		this.mEngine.getTextureManager().loadTexture(this.mTexture);
 	}
+
+	public void initMovingBall() {
+		this.mTexture = new Texture(256, 256, TextureOptions.DEFAULT);
+		this.mFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "gfx/bfs.png", 0, 0, 1, 1);
+
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+	}
+
+	// @Override
+	// public Scene onLoadScene() {
+	// this.mEngine.registerUpdateHandler(new FPSLogger());
+	//
+	// final Scene scene = new Scene();
+	// scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+	//
+	// final int centerX = (CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) /
+	// 2;
+	// final int centerY = (CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight())
+	// / 2;
+	// final Ball ball = new Ball(centerX, centerY, this.mFaceTextureRegion);
+	//
+	// scene.attachChild(ball);
+	//
+	// return scene;
+	// }
+
+//	@Override
+//	public Scene onLoadScene() {
+//		this.mEngine.registerUpdateHandler(new FPSLogger());
+//
+//		final Scene scene = new Scene();
+//		scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+//
+//		/*
+//		 * Calculate the coordinates for the face, so its centered on the
+//		 * camera.
+//		 */
+//		final int centerX = (CAMERA_WIDTH - this.mTextureRegion.getWidth()) / 2;
+//		final int centerY = (CAMERA_HEIGHT - this.mTextureRegion.getHeight()) / 2;
+//
+//		/* Create the face and add it to the scene. */
+//		final Sprite face = new Sprite(centerX, centerY, this.mTextureRegion) {
+//			// @Override
+//			// protected void applyRotation(final GL10 pGL) {
+//			// /* Disable culling so we can see the backside of this sprite. */
+//			// GLHelper.disableCulling(pGL);
+//			//
+//			// final float rotation = this.mRotation;
+//			//
+//			// if(rotation != 0) {
+//			// final float rotationCenterX = this.mRotationCenterX;
+//			// final float rotationCenterY = this.mRotationCenterY;
+//			//
+//			// pGL.glTranslatef(rotationCenterX, rotationCenterY, 0);
+//			// /* Note we are applying rotation around the y-axis and not the
+//			// z-axis anymore! */
+//			// pGL.glRotatef(rotation, 0, 1, 0);
+//			// pGL.glTranslatef(-rotationCenterX, -rotationCenterY, 0);
+//			// }
+//			// }
+//			//
+//			// @Override
+//			// protected void drawVertices(final GL10 pGL, final Camera pCamera)
+//			// {
+//			// super.drawVertices(pGL, pCamera);
+//			//
+//			// /* Enable culling as 'normal' entities profit from culling. */
+//			// GLHelper.enableCulling(pGL);
+//			// }
+//		};
+//		// face.registerEntityModifier(new LoopEntityModifier(new
+//		// RotationModifier(6, 0, 360)));
+//		scene.attachChild(face);
+//
+//		return scene;
+//	}
+	
 
 	@Override
 	public Scene onLoadScene() {
@@ -115,11 +235,13 @@ public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements
 		final Scene scene = new Scene();
 		scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
 
-		final int centerX = (CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
-		final int centerY = (CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight()) / 2;
-		final Ball ball = new Ball(centerX, centerY, this.mFaceTextureRegion);
+		/* Calculate the coordinates for the face, so its centered on the camera. */
+		final int centerX = (CAMERA_WIDTH - this.mTextureRegion.getWidth()) / 2;
+		final int centerY = (CAMERA_HEIGHT - this.mTextureRegion.getHeight()) / 2;
 
-		scene.attachChild(ball);
+		/* Create the face and add it to the scene. */
+		final Sprite face = new Sprite(centerX, centerY, this.mTextureRegion);
+		scene.attachChild(face);
 
 		return scene;
 	}
