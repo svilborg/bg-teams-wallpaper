0369b9a9cabb4f32440ec25b519e7cc459997ca7
Svilborg <svilborg@gmail.com>
2012-12-11 15:58:10 +0200

    Adding AndEngine 1 Functionality. Settings Template

--- src/comix/bgteams/app/BgTeamsWallpaperService.java
+++ src/comix/bgteams/app/BgTeamsWallpaperService.java
@@ -1,114 +1,182 @@
 package comix.bgteams.app;
 
-import android.os.Handler;
-import android.service.wallpaper.WallpaperService;
-import android.service.wallpaper.WallpaperService.Engine;
-import android.view.SurfaceHolder;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.handler.physics.PhysicsHandler;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.extension.ui.livewallpaper.BaseLiveWallpaperService;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.util.Debug;
 
-public class BgTeamsWallpaperService extends WallpaperService {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class BgTeamsWallpaperService extends BaseLiveWallpaperService implements SharedPreferences.OnSharedPreferenceChangeListener {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	public static final String SHARED_PREFS_NAME = "livewallpapertemplatesettings";
+
+	// Camera Constants
+
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	private static final float DEMO_VELOCITY = 100.0f;
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+
+	// Shared Preferences
+	private SharedPreferences mSharedPreferences;
+
+	private Texture mTexture;
+
+	private TiledTextureRegion mFaceTextureRegion;
+
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
 
 	@Override
-	public Engine onCreateEngine() {
-		return new BgTeamsEngine();
+	public org.anddev.andengine.engine.Engine onLoadEngine() {
+		// get screen size
+		// WindowManager windowManager = (WindowManager)
+		// getSystemService(Context.WINDOW_SERVICE);
+		// Display display = windowManager.getDefaultDisplay();
+		// DisplayMetrics displayMetrics = new DisplayMetrics();
+		// display.getMetrics(displayMetrics);
+		// int height = displayMetrics.heightPixels;
+		// int width = displayMetrics.widthPixels;
+		//
+		// CAMERA_WIDTH = width;
+		// CAMERA_HEIGHT = height;
+
+		Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new org.anddev.andengine.engine.Engine(new EngineOptions(true, ScreenOrientation.PORTRAIT, new FillResolutionPolicy(), mCamera));
 	}
 
-	class BgTeamsEngine extends Engine {
+	@Override
+	public void onLoadComplete() {
 
-		private Handler mHandler = new Handler();
+	}
 
-		private Runnable mIteration = new Runnable() {
-			public void run() {
-				iteration();
-				drawFrame();
+	@Override
+	protected void onTap(final int pX, final int pY) {
+
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences pSharedPrefs, String pKey) {
+
+	}
+
+	@Override
+	public void onLoadResources() {
+		//this.mTexture = new Texture(32, 32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+//		this.mFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "gfx/face_box.png", 0, 0, 0, 1);
+
+		//		this.mFaceTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/face_box.png");
+		
+		this.mTexture = new Texture(64, 64, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(this.mTexture, this, "gfx/face_box.png", 0, 0, 1, 1);
+
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene();
+		scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+
+		final int centerX = (CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
+		final int centerY = (CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight()) / 2;
+		final Ball ball = new Ball(centerX, centerY, this.mFaceTextureRegion);
+
+		scene.attachChild(ball);
+
+		return scene;
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+	private static class Ball extends AnimatedSprite {
+		private final PhysicsHandler mPhysicsHandler;
+
+		public Ball(final float pX, final float pY, final TiledTextureRegion pTextureRegion) {
+			super(pX, pY, pTextureRegion);
+
+			this.mPhysicsHandler = new PhysicsHandler(this);
+			this.registerUpdateHandler(this.mPhysicsHandler);
+
+			this.mPhysicsHandler.setVelocity(DEMO_VELOCITY, DEMO_VELOCITY);
+		}
+
+		@Override
+		protected void onManagedUpdate(final float pSecondsElapsed) {
+			if (this.mX < 0) {
+				this.mPhysicsHandler.setVelocityX(DEMO_VELOCITY);
+			} else if (this.mX + this.getWidth() > CAMERA_WIDTH) {
+				this.mPhysicsHandler.setVelocityX(-(DEMO_VELOCITY));
 			}
-		};
 
-		private boolean mVisible;
-
-		@Override
-		public void onDestroy() {
-			super.onDestroy();
-			// stop the animation
-			mHandler.removeCallbacks(mIteration);
-		}
-
-		@Override
-		public void onVisibilityChanged(boolean visible) {
-			mVisible = visible;
-			if (visible) {
-				iteration();
-				drawFrame();
-			} else {
-				// stop the animation
-				mHandler.removeCallbacks(mIteration);
+			if (this.mY < 0) {
+				this.mPhysicsHandler.setVelocityY(DEMO_VELOCITY);
+			} else if (this.mY + this.getHeight() > CAMERA_HEIGHT) {
+				this.mPhysicsHandler.setVelocityY(-(DEMO_VELOCITY));
 			}
-		}
 
-		@Override
-		public void onSurfaceChanged(SurfaceHolder holder, int format,
-				int width, int height) {
-			iteration();
-			drawFrame();
-		}
-
-		@Override
-		public void onSurfaceDestroyed(SurfaceHolder holder) {
-			super.onSurfaceDestroyed(holder);
-			mVisible = false;
-			// stop the animation
-			mHandler.removeCallbacks(mIteration);
-		}
-
-		@Override
-		public void onOffsetsChanged(float xOffset, float yOffset,
-				float xOffsetStep, float yOffsetStep, int xPixelOffset,
-				int yPixelOffset) {
-			iteration();
-			drawFrame();
-		}
-
-		protected void drawFrame() {
-		}
-
-		protected void iteration() {
-			// Reschedule the next redraw in 40ms
-			mHandler.removeCallbacks(mIteration);
-			if (mVisible) {
-				mHandler.postDelayed(mIteration, 1000 / 25);
-			}
+			super.onManagedUpdate(pSecondsElapsed);
 		}
 	}
+
+	@Override
+	public void onPauseGame() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onResumeGame() {
+		// TODO Auto-generated method stub
+	}
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
 }
-//
-// // private Aquarium _aquarium;
-//
-// public BgTeamsEngine() {
-// // this._aquarium = new Aquarium();
-// // this._aquarium.initialize(getBaseContext(), getSurfaceHolder());
-// }
-//
-// @Override
-// public void onVisibilityChanged(boolean visible) {
-// if (visible) {
-// // this._aquarium.render();
-// }
-// }
-//
-// @Override
-// public void onSurfaceChanged(SurfaceHolder holder, int format,
-// int width, int height) {
-// super.onSurfaceChanged(holder, format, width, height);
-// }
-//
-// @Override
-// public void onSurfaceCreated(SurfaceHolder holder) {
-// super.onSurfaceCreated(holder);
-// // this._aquarium.start();
-// }
-//
-// @Override
-// public void onSurfaceDestroyed(SurfaceHolder holder) {
-// super.onSurfaceDestroyed(holder);
-// // this._aquarium.stop();
-// }
-// }
